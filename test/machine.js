d50ec93e7b942514d369a6e8106f2838c283a995
ajlopez <webmaster@ajlopez.com>
2017-05-27 11:10:35 -0300

    Machine add, empty

--- test/machine.js
+++ test/machine.js
@@ -6,8 +6,27 @@ exports['push and pop'] = function (test) {
 	
 	mach.push(1);
 	mach.push(2);
+
+	test.equal(mach.empty(), false);
 	
 	test.equal(mach.pop(), 2);
 	test.equal(mach.pop(), 1);
+	
+	test.ok(mach.empty());
+};
+
+exports['add'] = function (test) {
+	var mach = machine();
+	
+	mach.push(1);
+	mach.push(2);
+
+	test.equal(mach.empty(), false);
+
+	mach.add();
+	
+	test.equal(mach.pop(), 3);
+	
+	test.ok(mach.empty());
 };
 
