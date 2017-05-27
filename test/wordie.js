e0a9878933512348df524d9dd5f57c6b578fc942
ajlopez <webmaster@ajlopez.com>
2017-05-27 18:23:04 -0300

    Execute a simple program

--- test/wordie.js
+++ test/wordie.js
@@ -5,3 +5,15 @@ exports['wordie as object'] = function (test) {
 	test.ok(wordie);
 	test.equal(typeof wordie, 'object');
 };
+
+exports['execute a simple program'] = function (test) {
+	var text = [
+		"the number one",
+		"the nightly moon",
+		"are all brigth",
+		"in rare sky"
+	].join('\r\n');
+
+	test.equal(wordie.execute(text), 3);
+};
+
