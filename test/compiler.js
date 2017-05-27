e359d1fffbc6723fd550cabbe27e0e5edac9f6ed
ajlopez <webmaster@ajlopez.com>
2017-05-27 11:49:39 -0300

    Compile string

--- test/compiler.js
+++ test/compiler.js
@@ -13,3 +13,13 @@ exports['compile spaces'] = function (test) {
 	test.equal(compiler.compile('    '), null);
 };
 
+exports['compile one word'] = function (test) {
+	test.equal(compiler.compile(' hi   '), null);
+};
+
+exports['compile string'] = function (test) {
+	test.equal(compiler.compile('a solid high elevator lifts light oxen'), 'machine.push("hello")');
+};
+
+
+
